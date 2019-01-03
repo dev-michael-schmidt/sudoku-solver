03f7c387ec19a66d85c296911515266f726c3323
Michael Schmidt <Michael.Schmidt82@gmail.com>
2019-01-02 23:26:20 -0600

    added deduce methods WIP

--- solver.py
+++ solver.py
@@ -1,3 +1,8 @@
+from itertools import chain, combinations
+
+def one_element_subset(ss):
+    return chain(*map(lambda x: combinations(ss, x), range(1, 2)))
+
 class AnnotatedBoard:
     def __init__(self):
         self.board = [[set({i for i in range(1, 10)}) for _ in range(9)] for _ in range(9)]
@@ -18,7 +23,7 @@ class AnnotatedBoard:
 
             if r % 3 == 2 and r != 8:
                 s += '\n------+-------+------\n'
-            elif r != 8:
+            else:
                 s += '\n'
         return s
 
@@ -30,38 +35,91 @@ class AnnotatedBoard:
                     if row[c] != '.':
                         self.board[r][int(c / 2)] = int(row[c])
 
-        self.row_iteration()
-        self.col_iteration()
-        self.block_iteration()
+        self.row_elimination()
+        self.col_elimination()
+        self.block_elimination()
 
-    def resolve(self):
-        action = False
+    def row_deduce(self):
         for r in range(9):
+            s = set({})
             for c in range(9):
                 if isinstance(self.board[r][c], set):
-                    if len(self.board[r][c]) == 1:
-                        self.board[r][c] = self.board[r][c].pop()
-                        action = True
+                    s = s.union(self.board[r][c])
 
-        return action
+            subsets = [set({e}) for e in s]
+            for subset in subsets:
+                flag = False
 
-    def deduce(self):
-        pass
+                for c in range(9):
+                    if isinstance(self.board[r][c], set):
+                        if subset.issubset(self.board[r][c]):
+                            if not flag:
+                                flag = True
+                            else:
+                                flag = False
+                                break
+                if flag:
+                    for c in range(9):
+                        if isinstance(self.board[r][c], set):
+                            if subset.issubset(self.board[r][c]):
+                                self.board[r][c] = subset
 
-    def solve(self):
+    def col_deduce(self):
+        for c in range(9):
+            s = set({})
+            for r in range(9):
+                if isinstance(self.board[r][c], set):
+                    s = s.union(self.board[r][c])
 
-        while(self.resolve()):
-            self.row_iteration()
-            self.col_iteration()
-            self.block_iteration()
+            subsets = [set({e}) for e in s]
+            for subset in subsets:
+                flag = False
+
+                for r in range(9):
+                    if isinstance(self.board[r][c], set):
+                        if subset.issubset(self.board[r][c]):
+                            if not flag:
+                                flag = True
+                            else:
+                                flag = False
+                                break
+                if flag:
+                    for r in range(9):
+                        if isinstance(self.board[r][c], set):
+                            if subset.issubset(self.board[r][c]):
+                                self.board[r][c] = subset
 
 
+    def block_deduce(self):
+        for row_start in range(0, 9, 3):
+            for col_start in range(0, 9, 3):
+                s = set({})
+                for r in range(row_start, row_start + 3):
+                    for c in range(col_start, col_start + 3):
+                        if isinstance(self.board[r][c], set):
+                            s = s.union(self.board[r][c])
 
+                subsets = [set({e}) for e in s]
+                for subset in subsets:
+                    flag = False
 
+                    for r in range(row_start, row_start + 3):
+                        for c in range(col_start, col_start + 3):
+                            if isinstance(self.board[r][c], set):
+                                if subset.issubset(self.board[r][c]):
+                                    if not flag:
+                                        flag = True
+                                    else:
+                                        flag = False
+                                        break
+                    if flag:
+                        for r in range(row_start, row_start + 3):
+                            for c in range(col_start, col_start + 3):
+                                if isinstance(self.board[r][c], set):
+                                    if subset.issubset(self.board[r][c]):
+                                        self.board[r][c] = subset
 
-
-
-    def row_iteration(self):
+    def row_elimination(self):
         for r in range(9):
             s = set({})
             for c in range(9):
@@ -72,7 +130,7 @@ class AnnotatedBoard:
                 if isinstance(self.board[r][c], set):
                     self.board[r][c] -= s
 
-    def col_iteration(self):
+    def col_elimination(self):
         for c in range(9):
             s = set({})
             for r in range(9):
@@ -83,7 +141,7 @@ class AnnotatedBoard:
                 if isinstance(self.board[r][c], set):
                     self.board[r][c] -= s
 
-    def block_iteration(self):
+    def block_elimination(self):
         for row_start in range(0, 9, 3):
             for col_start in range(0, 9, 3):
 
@@ -98,3 +156,30 @@ class AnnotatedBoard:
                     for c in range(col_start, col_start + 3):
                         if isinstance(self.board[r][c], set):
                             self.board[r][c] -= s
+
+    def resolve(self):
+        action = False
+        for r in range(9):
+            for c in range(9):
+                if isinstance(self.board[r][c], set):
+                    if len(self.board[r][c]) == 1:
+                        self.board[r][c] = self.board[r][c].pop()
+                        action = True
+
+        return action
+
+
+    def solve(self):
+        count = 0
+        while(self.resolve()):
+            self.row_deduce()
+            self.row_elimination()
+
+            self.col_deduce()
+            self.col_elimination()
+
+            #self.block_deduce()
+            self.block_elimination()
+            count +=1
+
+        print('total passes', count)
