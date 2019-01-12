bc2197255f38d8a0554db94c4d6186da53761699
Michael Schmidt <Michael.Schmidt82@gmail.com>
2019-01-12 13:48:12 -0600

    global includes

---
+++ globals.h
@@ -0,0 +1,15 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+#include <iostream>
+#include <new>
+#include <set>
+
+using std::bad_alloc;
+using std::cerr;
+using std::cout;
+using std::endl;
+using std::set;
+
+#endif // !GLOBALS_H
+
