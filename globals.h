a4af9377999e9fef36648952b875351b06dec189
Michael Schmidt <Michael.Schmidt82@gmail.com>
2019-01-12 14:49:06 -0600

    fstream for file reading added

--- globals.h
+++ globals.h
@@ -1,15 +1,18 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
 
+#include <fstream>
 #include <iostream>
 #include <new>
 #include <set>
+#include <string>
 
 using std::bad_alloc;
 using std::cerr;
 using std::cout;
 using std::endl;
+using std::fstream;
 using std::set;
+using std::string;
 
 #endif // !GLOBALS_H
-
