5a6da5286f82a69a2e896a88bca3cf05f679d472
Michael Schmidt <Michael.Schmidt82@gmail.com>
2019-01-12 14:48:32 -0600

    added basic elim functions

--- annotated_board.h
+++ annotated_board.h
@@ -8,6 +8,12 @@ class AnnotatedBoard {
 	public:
 		AnnotatedBoard();
 
+		bool from_file(string filename);
+
+		bool row_eliminate(int row = -1);
+		bool col_eliminate(int col = -1);
+		bool blk_eliminate(int row = -1, int col = -1);
+
 	private:
 		Cell** board;
 
